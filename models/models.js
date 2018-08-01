c5e407fca4e47612a603016fa2ee0b730e9c69c7
Amit Sant <amitsant.2000@gmail.com>
2018-08-01 15:13:49 -0700

    model changed

--- models/models.js
+++ models/models.js
@@ -2,8 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
   username:String,
-  hashedPassword:String,
-  keySalt:String
+  hashedPassword:String
 })
 
 var User = mongoose.model('User',userSchema);
