5836596c1390a518e6bf1fa47d6785745bcb2136
DavidStor <davidrusarm@gmail.com>
2018-08-01 16:19:47 -0700

    Changed the order of the index and passport stuff

--- app.js
+++ app.js
@@ -95,10 +95,9 @@ passport.deserializeUser((id, done) => {
 });
 app.use(passport.initialize());
 app.use(passport.session())
+app.use('/', auth(passport));
 app.use('/', index);
 app.use('/users', users);
-app.use('/', auth(passport));
-
 // catch 404 and forward to error handler
 
 // error handler
