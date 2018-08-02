7f7ee6fa6dd45473b71ee1e2c134ce0efa2150a0
Owen Zhang <owenzhang76@gmail.com>
2018-08-02 15:35:16 -0700

    stahp

--- models/models.js
+++ models/models.js
@@ -29,24 +29,12 @@ var userSchema = new mongoose.Schema({
   looks:[{
    type: mongoose.Schema.ObjectId,
    ref: 'Look'
-<<<<<<< HEAD
   }],
-  wardrobe: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Wardrobe'
-  }]
-=======
- }],
-<<<<<<< HEAD
  profilePic:String,
  wardrobe: [{
    type: mongoose.Schema.ObjectId,
    ref: 'Wardrobe'
  }]
-=======
- profilePic:String
->>>>>>> fd5ae9885831cd602a45230d762d19fba9c8b7bd
->>>>>>> 1f758bc1f4e151be539459c71226a84c777bc570
 });
 
 var postSchema = new mongoose.Schema({
@@ -98,7 +86,6 @@ var productSchema = new mongoose.Schema({
   price: Number,
   image: String,
 })
-<<<<<<< HEAD
 var wardrobeSchema = new mongoose.Schema({
  headwear: [{
    type: mongoose.Schema.ObjectId,
@@ -117,38 +104,11 @@ var wardrobeSchema = new mongoose.Schema({
    ref: 'Product'
  }]
 })
-=======
-<<<<<<< HEAD
 
-var wardrobeSchema = new mongoose.Schema({
-  headwear: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Product'
-  }]
-  tops: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Product'
-  }]
-  pants: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Product'
-  }]
-  footwear: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Product'
-  }]
-})
-
-=======
->>>>>>> fd5ae9885831cd602a45230d762d19fba9c8b7bd
->>>>>>> 1f758bc1f4e151be539459c71226a84c777bc570
 var User = mongoose.model('User', userSchema);
 var Post = mongoose.model('Post', postSchema);
 var Product = mongoose.model('Product', productSchema);
 var Look = mongoose.model('Look', lookSchema);
-<<<<<<< HEAD
-var Ratings = mongoose.model('Ratings',ratingsSchema)
-=======
 var Ratings = mongoose.model('Ratings',ratingsSchema);
 >>>>>>> 1f758bc1f4e151be539459c71226a84c777bc570
 var Wardrobe = mongoose.model('Wardrobe', wardrobeSchema);
