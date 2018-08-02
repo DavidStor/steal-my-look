ecf3846a0df46365456704c3c821ab0a3ee92b70
DavidStor <davidrusarm@gmail.com>
2018-08-01 19:17:05 -0700

    Fixed password hashing and simplefied it, fixed routs and registration for a user, also now all the errors can be visible

--- models/models.js
+++ models/models.js
@@ -1,11 +1,19 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema;
-var userSchema = new Schema({
-  username:String,
-  hashedPassword:String
-})
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
+mongoose.connect(connect);
 
-var User = mongoose.model('User',userSchema);
-module.exports = {
-  User:User
-}
+var userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+
+var User = mongoose.model('User', userSchema);
+export { User }
+
